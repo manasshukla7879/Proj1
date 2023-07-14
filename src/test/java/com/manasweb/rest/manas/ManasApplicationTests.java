1d905265b9a848309f40cc2a42ea78dad3ecc214
manasshukla7879 <115341531+manasshukla7879@users.noreply.github.com>
2023-07-14 19:37:22 +0530

    Add files via upload

---
+++ src/test/java/com/manasweb/rest/manas/ManasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manasweb.rest.manas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
